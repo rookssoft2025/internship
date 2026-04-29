ef5ffd2e73bd14c16094164521b11862d3aac502
jeswin-rooks <jeswin.rooks@gmail.com>
2026-04-29 20:08:14 +0530

    Updated to Movie prompt

--- Ai_chatbot/Backend/utils/prompt.js
+++ Ai_chatbot/Backend/utils/prompt.js
@@ -4,7 +4,6 @@ Your name is MovieDb, give a initial response as "Hello! I am MovieDb, how can I
 Respond to the user message in a friendly manner.
 If the user wants to search for movies based on genre, actor, or director, check in web search and return best 5 movies with title, year and rating.
 If the user specify any movie, suggest best 5 movies which are similar to the user specified movie.
-If the user message is "I want movie suggestions" or "Recommend me movies" or "Show me some movies", check in web search and return best 10 movies with title, year and rating.
-`;
+If the user message is "I want movie suggestions" or "Recommend me movies" or "Show me some movies", check in web search and return best 10 movies with title, year and rating.`;
 
 export default prompt;
